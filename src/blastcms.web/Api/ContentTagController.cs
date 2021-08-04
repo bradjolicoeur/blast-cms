833ffaa4f180748da8e20b1f2a27b907d0d94306
Brad Jolicoeur <brad@jolicoeurs.net>
2021-08-03 20:17:00 -0400

    add the rest of the api controllers

---
+++ src/blastcms.web/Api/ContentTagController.cs
@@ -0,0 +1,48 @@
+﻿using blastcms.web.Attributes;
+using blastcms.web.Data;
+using blastcms.web.Handlers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace blastcms.web.Api
+{
+    [Route("api")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiKey]
+    public class ContentTagController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ContentTagController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("contenttag/all")]
+        [Produces("application/json")]
+        [SwaggerOperation(
+            Summary = "Get Content Tags",
+            Description = "Returns a paged list of Content Tags",
+            OperationId = "GetContentTags",
+            Tags = new[] { "Content Tag" }
+        )]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(IEnumerable<ContentTag>))]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized, "Api Key is not valid")]
+        public async Task<ActionResult<IEnumerable<ContentTag>>> GetAll([FromQuery] int skip = 0, [FromQuery] int take = 10, [FromQuery] int currentPage=0)
+        {
+            var results = await _mediator.Send(new GetContentTags.Query(skip, take, currentPage));
+
+            return results.Data.ToArray();
+        }
+
+       
+
+    }
+}
---
+++ src/blastcms.web/Api/FeedArticleController.cs
@@ -0,0 +1,63 @@
+﻿using blastcms.web.Attributes;
+using blastcms.web.Data;
+using blastcms.web.Handlers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace blastcms.web.Api
+{
+    [Route("api")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiKey]
+    public class FeedArticleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FeedArticleController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("feedarticle/all")]
+        [Produces("application/json")]
+        [SwaggerOperation(
+            Summary = "Get Feed Articles",
+            Description = "Returns a paged list of Feed Articles",
+            OperationId = "GetFeedArticles",
+            Tags = new[] { "Feed Article" }
+        )]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(IEnumerable<FeedArticle>))]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized, "Api Key is not valid")]
+        public async Task<ActionResult<IEnumerable<FeedArticle>>> GetAll([FromQuery] int skip = 0, [FromQuery] int take = 10, [FromQuery] int currentPage=0, [FromQuery] string search=null)
+        {
+            var results = await _mediator.Send(new GetFeedArticles.Query(skip, take, currentPage, search));
+
+            return results.Data.ToArray();
+        }
+
+        [HttpGet("feedarticle/{id}")]
+        [Produces("application/json")]
+        [SwaggerOperation(
+            Summary = "Get Feed Article",
+            Description = "Returns a Feed Article",
+            OperationId = "GetFeedArticle",
+            Tags = new[] { "Feed Article" }
+        )]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(FeedArticle))]
+        [SwaggerResponse((int)HttpStatusCode.Unauthorized, "Api Key is not valid")]
+        public async Task<ActionResult<FeedArticle>> Get(Guid id)
+        {
+            var results = await _mediator.Send(new GetFeedArticle.Query(id));
+
+            return results.Data;
+        }
+
+    }
+}
