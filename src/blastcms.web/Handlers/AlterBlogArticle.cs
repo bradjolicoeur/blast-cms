676a9d4a52990abc90c04166a1dc9eb725ab1e38
Brad Jolicoeur <brad@jolicoeurs.net>
2021-08-29 14:27:15 -0400

    make published date editable

--- src/blastcms.web/Handlers/AlterBlogArticle.cs
+++ src/blastcms.web/Handlers/AlterBlogArticle.cs
@@ -4,6 +4,7 @@ using Marten;
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,13 +15,18 @@ namespace blastcms.web.Handlers
         public class Command : IRequest<Model>
         {
             public Guid? Id { get; set; }
+            [Required]
             public string Title { get; set; }
             public string Author { get; set; }
             public HashSet<String> Tags { get; set; }
-            public DateTime PublishedDate { get; set; }
+
+            [Required]
+            public DateTime? PublishedDate { get; set; }
             public ImageFile Image { get; set; }
             public string Description { get; set; }
             public string Body { get; set; }
+
+            [Required]
             public string Slug { get; set; }
 
         }
@@ -58,7 +64,6 @@ namespace blastcms.web.Handlers
             public async Task<Model> Handle(Command request, CancellationToken cancellationToken)
             {
                 var article = _mapper.Map<BlogArticle>(request);
-                article.PublishedDate = DateTime.UtcNow;
 
                 using var session = _sessionFactory.OpenSession();
                 {
