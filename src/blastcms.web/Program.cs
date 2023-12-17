154284216d25a04c95b1617cb2bb8e5a675a4626
Brad Jolicoeur <brad@jolicoeurs.net>
2023-12-17 13:51:45 -0500

    force tls 1.2

--- src/blastcms.web/Program.cs
+++ src/blastcms.web/Program.cs
@@ -5,14 +5,18 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace blastcms.web
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
             CreateHostBuilder(args).Build().Run();
         }
 
