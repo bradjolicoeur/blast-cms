b790b6e9ff5aa4e24dd285a936d9f41471d328b7
Brad Jolicoeur <brad@jolicoeurs.net>
2025-12-27 12:27:29 -0500

    replace startup with program.cs to use a more modern pattern

--- src/blastcms.web/Helpers/UrlHelper.cs
+++ src/blastcms.web/Helpers/UrlHelper.cs
@@ -1,14 +1,18 @@
 ﻿
 
+using Microsoft.Extensions.Configuration;
+
 namespace blastcms.web.Helpers
 {
     public static class UrlHelper
     {
         public const string IMAGE_BASE_URL = "https://storage.googleapis.com";
+        
+        public static IConfiguration Configuration { get; set; }
 
         public static string GetFullImageUrl(this string imagePath)
         {
-            var bucket = Startup.Configuration["GoogleCloudStorageBucket"];
+            var bucket = Configuration["GoogleCloudStorageBucket"];
             return $"{IMAGE_BASE_URL}/{bucket}/{imagePath}";
         }
     }
